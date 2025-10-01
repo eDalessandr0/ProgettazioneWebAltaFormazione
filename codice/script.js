526e899b55a0429e39f07334e7ce3bdf50bc3820
dario.dalessandro <dario.dalessandro@marconirovereto.it>
2025-10-01 15:17:41 +0200

    commit detto commit ma chi fa i commit committa

---
+++ codice/script.js
@@ -0,0 +1,17 @@
+/*script js scrivere app sulla repo appunti pls*/
+/*Dichiarazione di variabili*/
+var x = "y";
+
+let y = "x";
+let eta = 19;
+/*constante (non può essere cambiata)
+La nomenclatura delle constanti è Snake_Case  (Possibilmente in maiuscolo)
+*/
+const COSTANTE_DI_DARIO = 7;
+
+
+function clickFunction(){
+    console.log("hai cliccato un bottone");
+    console.log("Costante di dario:" + COSTANTE_DI_DARIO);
+
+}
